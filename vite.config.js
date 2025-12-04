ad3b10b18ba59856a5fd151a83b5ad22ce7d1964
Ivan Santos <alejandrosantis93@gmail.com>
2025-12-04 17:07:17 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 import path from 'path'
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    allowedHosts: ['proteccion-de-rutas.onrender.com']
+  }
+})
 
 export default defineConfig({
   plugins: [
